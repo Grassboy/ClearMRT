798d559ca90bfe17b16f8dbd7d1a4ba1ad7ee7c8
grassboy <ClickMRT@3x.cm>
2015-11-07 04:31:22 +0800

    add config sample

---
+++ config/config.sample.php
@@ -0,0 +1,12 @@
+<?
+	$_title = "ClickMRT";
+	$_thumb = "http://i.imgur.com/GFpM7P2.png";
+	$_description = "讓你快速查詢各站間票價、行車時間的捷運資訊小幫手";
+	$_url = "http://grassboy.tw/ClickMRT/";
+    $_fb_admin_id = 663898857;
+    
+	$_relate_link = "http://www.plurk.com/p/koh46o";
+	$_plurk_status = "{href} ".$_description." {img}";
+	$_plurk_img = "http://i.imgur.com/MsZmpVw.png";
+	$_type = "website";
+?>
